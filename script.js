24d8a060c0a7a1aa298c6bad316118e5ea69c525
Maciejaszek1 <maciek.sygula@gmail.com>
2023-04-03 13:50:27 +0200

    Cards display books from array

--- script.js
+++ script.js
@@ -18,10 +18,24 @@ function addBookToLibrary () {
     let pages = prompt("Number of pages:");
     let read = prompt("Is the book read?");
 
+    const postCards = document.querySelector('.cards');
+
     const newBook = new Book(name,author,pages,read);
     library.push(newBook);
 
     console.log(newBook.info());
+
+    const bookNum = library.length - 1;
+    const postElement = document.createElement('div');
+    postElement.classList.add('card');
+    postElement.innerHTML = 
+    `
+        <h1>${library[bookNum].name}</h1>
+        <h3>${library[bookNum].author}</h3>
+        <p>${library[bookNum].pages}</p>
+    `;
+
+    postCards.appendChild(postElement)
 
 }
 
