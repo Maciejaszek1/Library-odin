7e77b0282a74503ce3002e1b4589c2ba8fdd02c2
Maciejaszek1 <maciek.sygula@gmail.com>
2023-04-08 15:57:16 +0200

    Modal completed

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 let library = [];
 
-const addBookButton = document.getElementById('addBook');
-const modal = document.getElementById("modal");
+const showModal = document.getElementById('addBook');
+const addButton = document.getElementById('addButton');
+showModal.onclick = function() {
+    modal.style.display = "block";
+}
 
-function Book(name, author, pages, read) {
-    this.name = name;
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function info() {
-        return name + " by " + author + ", " + pages + " pages, " + read; 
+        return title + " by " + author + ", " + pages + " pages, " + read; 
     }
 }
 
 function addBookToLibrary () {
-    // let name = prompt("Name of the book:");
-    // let author = prompt("Author:");
-    // let pages = prompt("Number of pages:");
-    // let read = prompt("Is the book read?");
-    let name = document.getElementById("title");
-    let author = document.getElementById("author");
-    let pages = document.getElementById("pages");
-    modal.style.display = "block";
-    
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").value;
     const postCards = document.querySelector('.cards');
-
-    const newBook = new Book(name,author,pages,read);
+    
+    const newBook = new Book(title, author, pages, read);
     library.push(newBook);
-
-    console.log(newBook.info());
 
     const bookNum = library.length - 1;
     const postElement = document.createElement('div');
+    
     postElement.classList.add('card');
     postElement.innerHTML = 
     `
-        <h1>${library[bookNum].name}</h1>
+        <h1>${library[bookNum].title}</h1>
         <h3>${library[bookNum].author}</h3>
         <p>${library[bookNum].pages}</p>
+        <p>${library[bookNum].read}</p>
     `;
 
-    postCards.appendChild(postElement)
-
+    postCards.appendChild(postElement);
+    modal.style.display = "none";
 }
 
-addBookButton.addEventListener('click', addBookToLibrary);
+addButton.addEventListener('click', addBookToLibrary);
+
+
