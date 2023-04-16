1b7feb5fcacad1a9ac6bc61ae4c5e453f5789fd0
Maciejaszek1 <maciek.sygula@gmail.com>
2023-04-16 21:06:36 +0200

    Finished Read button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let library = [];
 const showModal = document.getElementById('addBook');
 const addButton = document.getElementById('addButton');
 let deleteButtons = document.querySelectorAll('.delete');
+let readButtons = document.querySelectorAll('.readButton');
 
 showModal.onclick = function() {
     modal.style.display = "block";
@@ -22,7 +23,7 @@ function addBookToLibrary () {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
-    const read = document.getElementById("read").value;
+    let read = document.querySelector("input[name=finished]:checked").value;
     const postCards = document.querySelector('.cards');
     
     const newBook = new Book(title, author, pages, read);
@@ -30,7 +31,7 @@ function addBookToLibrary () {
 
     const bookNum = library.length - 1;
     const postElement = document.createElement('div');
-    
+
     postElement.classList.add('card');
     postElement.setAttribute('id',bookNum);
     postElement.innerHTML = 
@@ -40,16 +41,23 @@ function addBookToLibrary () {
             <button class="delete">&#215</button>
         </div>
         <h3>${library[bookNum].author}</h3>
-        <p>${library[bookNum].pages} pages</p>
+        <div class="cardFooter">
+            <p>${library[bookNum].pages} pages</p>
+            <button class="readButton">${library[bookNum].read}</button>
+        </div>
     `;
 
     postCards.appendChild(postElement);
     modal.style.display = "none";
-    console.log(library);
-    console.log(deleteButtons);
+
     deleteButtons = document.querySelectorAll('.delete');
+    readButtons = document.querySelectorAll('.readButton');
+
     deleteButtons.forEach(button => {
         button.addEventListener('click', deleteBook);
+    });
+    readButtons.forEach(button => {
+        button.addEventListener('click', readStatus);
     });
 }
 
@@ -57,17 +65,38 @@ function deleteBook(event) {
     const button = event.target;
     const card = button.parentNode.parentNode;
     const id = card.getAttribute('id');
+
     for (let i = id; i < library.length; i++) {
         const card = document.getElementById(i);
         card.setAttribute('id', i - 1);
     }
+
     card.parentNode.removeChild(card);
     library.splice(id, 1);
     console.log(library);
-
 }
 
+function readStatus(event) {
+    const button = event.target;
+    const card = button.parentNode.parentNode;
+    const id = parseInt(card.getAttribute('id'));
+    
+    if (button.textContent == 'Read') {
+        button.textContent = 'Unread';
+        library[id].read = 'Unread';
+    } else {
+        button.textContent = 'Read'; 
+        library[id].read = 'Read';
+    }
+
+    console.log(library[id].read);
+    console.log(library);
+}
+
+addButton.addEventListener('click', addBookToLibrary);
 deleteButtons.forEach(button => {
     button.addEventListener('click', deleteBook);
 });
-addButton.addEventListener('click', addBookToLibrary);
+readButtons.forEach(button => {
+    button.addEventListener('click', readStatus);
+});
